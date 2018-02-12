9e392956883fecb6049d18649e7f8a717a071fab
Daniel Alba Diaz <danialba96@gmail.com>
2018-02-12 10:08:06 +0100

    y

--- desplegar/despliegue.sh
+++ desplegar/despliegue.sh
@@ -9,4 +9,4 @@ rm -rf docs
 rm -rf despliegue
 docker run -d -p 80:8080 --net=red_docker --ip 192.168.1.68 --name gospo -v /opt/gospo/www:/var/www logongas/apache2-php7-ssl
 docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.69 --name gospo-mysql -v /opt/gospo/mysql:/var/lib/mysql -v /opt/gospo/database:/database  -e MYSQL_ROOT_PASSWORD=root mysql
-docker exec -t gospo-mysql /bin/bash /opt/gospo/database/import.sh
+docker exec -t gospo-mysql /bin/bash /database/import.sh
