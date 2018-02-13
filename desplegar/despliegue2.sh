e242be3956f546f0039fec5e383ac898c9e1449f
root <root@localhost.localdomain>
2018-02-13 11:26:10 +0100

    djdjd

--- desplegar/despliegue2.sh
+++ desplegar/despliegue2.sh
@@ -8,4 +8,4 @@ rm -rf /opt/gospo/docs
 rm -rf /opt/gospo/despliegue
 docker run -d -p 80:8080 --net=red_docker --ip 192.168.1.68 --name gospo2 -v /opt/gospo/www:/var/www logongas/apache2-php7-ssl
 docker run -d -p 3306:3306 --net=red_docker --ip 192.168.1.69 --name gospo2-mysql -v /opt/gospo/mysql:/var/lib/mysql -v /opt/gospo/database:/database -e MYSQL_ROOT_PASSWORD=root mysql
-docker exec -t gospo2-mysql /bin/bash /database/import.sh
+docker exec -i -t gospo2-mysql /bin/bash /database/import.sh
